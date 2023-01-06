3e9da500daad036362208e1399d33e775fbaa14a
이현규 <squareworld@naver.com>
2023-01-06 09:28:43 +0900

    23.01.06 config, env로 서버 설정 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,30 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './post/entities/post.entity';
+import { ConfigModule } from '@nestjs/config';
+import { MySqlConfigModule } from './config/database/config.module';
+import { MySqlConfigService } from './config/database/config.service';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     PostModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'blog',
-      entities: [Post],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.dev.env', '.prod.env'],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
     }),
   ],
 })
